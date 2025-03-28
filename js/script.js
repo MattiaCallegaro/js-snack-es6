b3c1c810c3619ec820febc9e44faf97b7e9f4448
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-28 15:46:23 +0100

    [DONE] esercizio finito

--- js/script.js
+++ js/script.js
@@ -3,35 +3,35 @@
 
 // creo un array di oggetti
 
-const biciDaCorsa =[
+const biciDaCorsa = [
     {
-        nome:"Pinnarello",
-        peso:6.8,
+        nome: "Pinnarello",
+        peso: 6.8,
     },
     {
-        nome:"Van Rysel",
-        peso:6,
+        nome: "Van Rysel",
+        peso: 6,
     },
     {
-        nome:"E-gravel",
-        peso:5.4,
+        nome: "E-gravel",
+        peso: 5.4,
     },
     {
-        nome:"Tarmac",
-        peso:5,
+        nome: "Tarmac",
+        peso: 5,
     }
 
 ]
 
 // ciclo per trovare il peso minore
-let pesoMin=biciDaCorsa[0].peso;
-let biciLeggera =biciDaCorsa[0] ;
+let pesoMin = biciDaCorsa[0].peso;
+let biciLeggera = biciDaCorsa[0];
 
-for(let i=0; i<biciDaCorsa.length; i++){
+for (let i = 0; i < biciDaCorsa.length; i++) {
     // console.log(biciDaCorsa[i.peso])
-    if(biciDaCorsa[i].peso < pesoMin){
-       pesoMin = biciDaCorsa[i].peso;
-       biciLeggera = biciDaCorsa[i]
+    if (biciDaCorsa[i].peso < pesoMin) {
+        pesoMin = biciDaCorsa[i].peso;
+        biciLeggera = biciDaCorsa[i]
 
     }
 }
@@ -42,26 +42,26 @@ console.log(`La  bici più leggera è ${biciLeggera.nome} con un peso di ${pesoM
 
 // creo un array di oggetti
 
-const squadreDiCalcio =[
+const squadreDiCalcio = [
     {
-        nome:"Milan",
-        punti_fatti:0,
-        falli_subiti:0
+        nome: "Milan",
+        punti_fatti: 0,
+        falli_subiti: 0
     },
     {
-        nome:"Inter",
-        punti_fatti:0,
-        falli_subiti:0
+        nome: "Inter",
+        punti_fatti: 0,
+        falli_subiti: 0
     },
     {
-        nome:"Juventus",
-        punti_fatti:0,
-        falli_subiti:0
+        nome: "Juventus",
+        punti_fatti: 0,
+        falli_subiti: 0
     },
     {
-        nome:"Napoli",
-        punti_fatti:0,
-        falli_subiti:0
+        nome: "Napoli",
+        punti_fatti: 0,
+        falli_subiti: 0
     },
 ]
 
@@ -69,17 +69,28 @@ console.log(squadreDiCalcio)
 
 //genero numeri casuali
 const generateRandomNumber = () => {
-    return Math.floor(Math.random() *( 15 - 10 + 1)) + 1;
+    return Math.floor(Math.random() * (15 - 10 + 1)) + 1;
 }
 
 // ciclo l'array per far in mododi ottenere numeri casuali per punti e falli
-for( let i=0; i<squadreDiCalcio.length; i++){
+for (let i = 0; i < squadreDiCalcio.length; i++) {
     squadreDiCalcio[i].punti_fatti = generateRandomNumber();
     squadreDiCalcio[i].falli_subiti = generateRandomNumber();
 }
 
 console.log(squadreDiCalcio);
 
-for( let i=0; i<squadreDiCalcio.length; i++){
+//creo un nuovo array per solo nomi e falli
+const squadraFalli = [];
+for (let i = 0; i < squadreDiCalcio.length; i++) {
+
+    const nomeFalli = {
+        nome: squadreDiCalcio[i].nome,
+        falli_subiti: squadreDiCalcio[i].falli_subiti
+    }
+
+    squadraFalli.push(nomeFalli);
 
 }
+
+console.log(squadraFalli)
