3c0e738db05f16cc0493581a886a37bb2b6db1a8
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-03-28 14:08:40 +0100

    [UPD] Create array

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// creo un array obj
+
+const biciDaCorsa =[
+    {
+        nome:"Pinnarello",
+        peso:"6.4 kg",
+    },
+    {
+        nome:"Pinnarello",
+        peso:"7.2 kg",
+    },
+    {
+        nome:"Pinnarello",
+        peso:"8.0kg",
+    },
+    {
+        nome:"Pinnarello",
+        peso:"5.9 kg",
+    }
+
+]
+
+console.log(biciDaCorsa)
